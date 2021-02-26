e1f17892aba1cda1cc90e6f0a3ddee43d6ef5f03
Gabriel Vega <gvega001@gmail.com>
2021-02-25 17:18:48 -0800

    Created a product class with props name, color, and size.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,24 @@ namespace DesignPatterns
         Small, Medium, Large
     }
 
+    public class Product
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
     public class Demo
     {
         static void Main(string[] args)
