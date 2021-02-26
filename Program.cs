856f35a7e4ee740586e1d67100599491ff0c7647
Gabriel Vega <gvega001@gmail.com>
2021-02-25 17:15:11 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public enum Color
+    {
+        Red, Green, Blue, Black, Orange
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
